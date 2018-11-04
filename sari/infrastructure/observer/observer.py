fb8f568bef7376cad1911722e9d7d7efe953954c
eqalbuquerque <emanuelqalbuquerque@gmail.com>
2018-11-04 15:27:04 -0300

    (Housekeeping) corrigindo typo

--- sari/infrastructure/observer/observer.py
+++ sari/infrastructure/observer/observer.py
@@ -1,6 +1,6 @@
 class MainObserver(object):
   def __init__(self, observado):
-    observado.register(self)  # REGISTRAR O OBSERVADOR NO OBSERVADO
+    observado.registrar(self)  # REGISTRAR O OBSERVADOR NO OBSERVADO
 
   def notify(self, observado, *args):
     print(f"{type(self).__name__}: Recebeu Comando {args} de {observado}")
