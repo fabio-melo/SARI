222242338e37ffb829cd86cccf12e90d5edbe942
Thuane França <44276812+thuane-franca@users.noreply.github.com>
2018-11-04 17:43:37 -0300

    (Memento) implementando o memento das ultimas 10 modificacoes

---
+++ sari/infrastructure/memento/snapshot.py
@@ -0,0 +1,20 @@
+import pickle
+
+class SnapshotMemento(object):
+  def __init__(self, maximo=10):
+    self.estados = []
+    self.maximo = maximo
+
+  def criar_snapshot(self, estado):
+    if len(self.estados) < self.maximo:
+      self.estados.append(pickle.dump(estado))
+    else:
+      self.estados.pop(0)
+      self.estados.append(pickle.dump(self.estados))
+
+  def restaurar_ultimosnapshot(self):
+    try:
+      return pickle.loads(self.estados.pop())
+    except:
+      print("Não há estados para restaurar")
+  
