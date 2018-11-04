941843cc3da5514c87c04c6b56fdfa63e77d6729
Fábio Melo <fabiomello@me.com>
2018-11-04 15:38:33 -0300

    (Command) Criando o Invoker, Integrando com Observer

---
+++ sari/infrastructure/command/invoker.py
@@ -0,0 +1,25 @@
+from sari.infrastructure.observer.receiver import Receiver
+
+class CommandInvoker():
+  def __init__(self):
+    self.__fila_comandos = []
+    self.receiver = Receiver()
+
+  
+  def enfileirar(self, comando):
+    '''Adiciona um Comando na fila '''
+    self.__fila_comandos.append(comando)
+
+  def executar_fila(self):
+    ''' Executa tudo que está na fila e zera a fila '''
+    for comando in self.__fila_comandos:
+      self.receiver.notificar_todos(f"Executando Comando {comando}")
+      comando.execute()
+
+    self.__fila_comandos = []
+  
+  def executar_imediatamente(self, comando):
+    self.receiver.notificar_todos(f"Executando Comando {comando}")
+    comando.execute()
+
+
